b1fdba57f4a19061700597c4032894a1ebd0e434
YOON-CC <whdbscks77@gmail.com>
2023-08-21 13:47:15 +0900

    commit

---
+++ ts_animation/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Animation_1 from './ts_animation_page/animation_1';
+import Animation_2 from './ts_animation_page/animation_2';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Animation_1></Animation_1> */}
+      <Animation_2></Animation_2>
+    </div>
+  );
+}
+
+export default App;
